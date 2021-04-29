8e3ae7a6bc71e73246f70eb16271ddb1c667b85f
Caique Tuon <tuoncaique.gmail.com>
2021-04-29 10:27:49 -0300

    Colocando components no projeto

--- recycleme/src/app/app.module.ts
+++ recycleme/src/app/app.module.ts
@@ -4,10 +4,32 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { MenuComponent } from './menu/menu.component';
+import { EntrarComponent } from './entrar/entrar.component';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { CategoriaComponent } from './categoria/categoria.component';
+import { CadastrarProdutoComponent } from './cadastrar-produto/cadastrar-produto.component';
+import { HomeLoginComponent } from './home-login/home-login.component';
+import { ProdutoDetalheComponent } from './produto-detalhe/produto-detalhe.component';
+import { EditarPerfilComponent } from './editar-perfil/editar-perfil.component';
+import { ProdutoAvaliacaoComponent } from './produto-avaliacao/produto-avaliacao.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    EntrarComponent,
+    CadastrarComponent,
+    FooterComponent,
+    HomeComponent,
+    CategoriaComponent,
+    CadastrarProdutoComponent,
+    HomeLoginComponent,
+    ProdutoDetalheComponent,
+    EditarPerfilComponent,
+    ProdutoAvaliacaoComponent
   ],
   imports: [
     BrowserModule,
---
+++ recycleme/src/app/produto-avaliacao/produto-avaliacao.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-produto-avaliacao',
+  templateUrl: './produto-avaliacao.component.html',
+  styleUrls: ['./produto-avaliacao.component.css']
+})
+export class ProdutoAvaliacaoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
